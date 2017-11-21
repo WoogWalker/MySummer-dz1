0afdd7b2f121e17cee66ad85d86e03e632947cb5
WoogWalker <Richard.Shneyder.77@gmail.com>
2017-11-21 02:53:45 +0200

    common base of the project

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc =  new Scanner(System.in);
+        int selectMonth = 0;
+        boolean menu = true;
+
+        while (menu){
+            System.out.println("Как я провел свое лето 2017.\nРассказ делится на 3 части, 3 месяца.\nВведи цифру 1-3");
+            switch (selectMonth){
+                case 1:
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+            }
+        }
     }
 }
